6d961f4e0dbb4e52a41b9cd2d26d1c77d016e000
ellison <ellison_wu@126.com>
2020-05-21 11:11:13 +0800

    添加项目文件。

---
+++ Config/ConfigValue.cs
@@ -0,0 +1,31 @@
+﻿using Aliyun.Acs.Core.Profile;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace wuwh.DdnsSvr.Config
+{
+    public class ConfigValue
+    {
+        /// <summary>
+        /// 默认地域
+        /// </summary>
+        public static string RegionId { get; set; } = "cn-shenzhen";
+        /// <summary>
+        /// 获取客户端凭证
+        /// </summary>
+        /// <param name="regionId">地域ID</param>
+        /// <param name="accessKeyId"></param>
+        /// <param name="secret"></param>
+        /// <returns></returns>
+        public static IClientProfile GetClientProfile(string regionId, string accessKeyId, string secret)
+        {
+            return DefaultProfile.GetProfile(regionId, accessKeyId, secret);
+        }
+        public static IClientProfile GetClientProfile( string accessKeyId, string secret)
+        {
+            return DefaultProfile.GetProfile(RegionId, accessKeyId, secret);
+        }
+    }
+}
---
+++ Controllers/AliDnsController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Aliyun.Acs.Alidns.Model.V20150109;
+using Aliyun.Acs.Core;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using wuwh.DdnsSvr.Config;
+
+namespace wuwh.DdnsSvr.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AliDnsController : ControllerBase
+    {
+        [HttpGet("UpdateIp")]
+        public async Task<IActionResult> UpdateIp(string domain,string accessKeyId, string secret,string ip)
+        {
+            try
+            {
+                var index = domain.IndexOf('.');
+                if (index < 1) return BadRequest("域名格式错误");
+                var subDomainName = domain.Substring(0, index);
+                var domainName = domain.Substring(index + 1, domain.Length - index - 1);
+                DefaultAcsClient client = new DefaultAcsClient(ConfigValue.GetClientProfile(accessKeyId, secret));
+                DescribeDomainRecordsRequest request = new DescribeDomainRecordsRequest();
+                request.DomainName = domainName;
+                DescribeDomainRecordsResponse response = await Task.Run(() => client.GetAcsResponse(request));
+                if (response.TotalCount > 0)
+                {
+                    var subDomainRecordList = response.DomainRecords.Where(t => t.RR == subDomainName);
+                    if (subDomainRecordList.Count() < 1) return BadRequest("子域名不存在");
+                    var subDomainRecord = subDomainRecordList.First();
+                    if (subDomainRecord._Value == ip) return new JsonResult(new { status = "success", msg = "IP未发生变动，无需更新" });
+                    UpdateDomainRecordRequest upRequest = new UpdateDomainRecordRequest();
+                    upRequest.RR = subDomainName;
+                    upRequest.RecordId = subDomainRecord.RecordId;
+                    upRequest.Type = "A";
+                    upRequest._Value = ip;
+                    UpdateDomainRecordResponse upRes = await Task.Run(() => client.GetAcsResponse(upRequest));
+
+                    return new JsonResult(new { status = "success", msg = $"IP更新成功，ID为：{upRes.RequestId}" });
+                }
+                return new JsonResult(new { status = "fail", msg = "未获取到域名解析列表" });
+            }
+            catch (Exception ex)
+            {
+                return new JsonResult(new { status = "fail", msg = ex.Message});
+            }
+        }
+    }
+}
